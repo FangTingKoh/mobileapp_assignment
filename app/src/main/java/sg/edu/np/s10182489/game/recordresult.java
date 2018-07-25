ca74bcc04132e57bf92452019f21af79152776c8
Emily Koh <s10182489@connect.np.edu.sg>
2018-07-25 15:11:37 +0800

    Merge branch 'master' of https://github.com/FangTingKoh/mobileapp_assignment
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/java/sg/edu/np/s10182489/game/recordresult.java

--- app/src/main/java/sg/edu/np/s10182489/game/recordresult.java
+++ app/src/main/java/sg/edu/np/s10182489/game/recordresult.java
@@ -5,32 +5,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class recordresult extends AppCompatActivity {
 
-    TextView scoreLabel;
-    TextView highScoreLabel;
-    Button viewScore;
-
-    DatabaseReference databaseScore;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recordresult);
 
         TextView scoreLabel = (TextView)findViewById(R.id.scoreLabel);
         TextView highScoreLabel = (TextView)findViewById(R.id.highScoreLabel);
-
-        databaseScore = FirebaseDatabase.getInstance().getReference("scores");
 
         int score = getIntent().getIntExtra("SCORE", 0);
 
@@ -40,57 +26,20 @@ public class recordresult extends AppCompatActivity {
 
         int highScore = settings.getInt("HIGH_SCORE", 0);
 
-        if(score > highScore)
-        {
-            highScoreLabel.setText("High sg.edu.np.s10182489.game.Score : " + score);
+        if(score > highScore){
+            highScoreLabel.setText("High Score : " + score);
 
 //save
             SharedPreferences.Editor editor = settings.edit();
             editor.putInt("HIGH_SCORE", score);
             editor.commit();
+        }else{
+            highScoreLabel.setText("High Score: " + highScore);
         }
-        else
-            {
-
-            highScoreLabel.setText("High sg.edu.np.s10182489.game.Score: " + highScore);
-
-            }
-
-        viewScore.setOnTouchListener(new View.OnTouchListener()
-        {
-            @Override
-            public boolean onTouch(View v, MotionEvent event)
-            {
-                return false;
-            }
-        });
-    }
-
-    //method to add score  into the database
-    private void addScore()
-    {
-        //Value of the score
-        String Score = scoreLabel.getText().toString();
-        if(!TextUtils.isEmpty(Score))
-        {
-            //to generate the unique key and get the key form the database.
-            String scoreID = databaseScore.push().getKey();
-
-            Score score = new Score(Score);
-
-            databaseScore.child(scoreID).setValue(Score);
-        }
-
     }
     public void tryAgain(View view)
     {
         startActivity(new Intent(getApplicationContext(), menu.class));
     }
-    /*
-    public void viewScore(View view)
-    {
-        startActivity(new Intent(getApplicationContext(), game_recyclerview.class));
-    }
-    */
-
 }
+
