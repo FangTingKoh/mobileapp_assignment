80c69d7ee4bdf35734d46ea87ca41ce75e40d6f5
Emily Koh <s10182489@connect.np.edu.sg>
2018-07-25 15:06:25 +0800

    Merge branch 'master' of https://github.com/FangTingKoh/mobileapp_assignment
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/java/sg/edu/np/s10182489/game/recordresult.java

--- app/src/main/java/sg/edu/np/s10182489/game/instruction.java
+++ app/src/main/java/sg/edu/np/s10182489/game/instruction.java
@@ -1,13 +1,52 @@
 package sg.edu.np.s10182489.game;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
 
 public class instruction extends AppCompatActivity {
+
+    Button flash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable(){
+            public void run(){
+                startFlash();
+            }
+        }, 3000);
+    }
+
+    public void startFlash()
+    {
+        flash=(Button)findViewById(R.id.playbutton);
+
+        Animation blinkanimation = new AlphaAnimation(1, 0);
+
+        blinkanimation.setDuration(200);
+
+        blinkanimation.setInterpolator(new LinearInterpolator());
+
+        blinkanimation.setRepeatCount(5);
+
+        blinkanimation.setRepeatMode(Animation.REVERSE);
+
+        flash.startAnimation(blinkanimation);
+    }
+
+    public void onClick(View v)
+    {
+        Intent in = new Intent(this,MainActivity_game.class);
+        startActivity(in);
     }
 }
